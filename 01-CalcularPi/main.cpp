c3388580f79f3eaec83ecd4bd84d4de9bc1c55ad
Marcos Nicolás Poveda <134090810+MarcosNicolasPoveda@users.noreply.github.com>
2023-05-21 18:18:33 -0300

    Add files via upload

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 int main()
 {
-    double pi{0.0};
+    double pi{0};
     for (int n = 0; n < 1000000 ; n++)
     { 
         pi += (pow(-1,n))/ (2*n + 1);
     }
     pi = pi*4.00;
     cout.precision(6);
-    cout << fixed << pi ;
+    cout << "El valor de pi es: " << fixed << pi <<endl;
     
         return 0;
 }
